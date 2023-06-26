7e822fc25e908716252385002f884ee6d60b2bae
Vicki Fraga <vicki4652@gmail.com>
2023-06-26 14:21:52 -0600

    [ADD] Commit without fear

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # random-str-msg-vf
 Random String Messages
+
+
+## Install
+
+```npm
+npm install -g random-str-msg
+```
+
+# Usage
+
+```bash
+ random-str-msg
+```
+
+# Contributing
+
+If someone wants to add or improve something, I invite you to collaborate directly in this repository: [random-str-msg-vf](https://github.com/vickifraga/random-str-msg-vf)
+
+# License
+
+funny-commit is released under the [MIT License](https://opensource.org/licenses/MIT)
---
+++ src/index.js
@@ -0,0 +1,27 @@
+const messages = [
+  "This is where it all begins...",
+  "Commit committed",
+  "Version control is awful",
+  "COMMIT ALL THE FILES!",
+  "The same thing we do every night, Pinky - try to take over the world!",
+  "Lock S-foils in attack position",
+  "This commit is a lie",
+  "I'll explain when you're older!",
+  "Here be Dragons",
+  "Reinventing the wheel. Again.",
+  "This is not the commit message you are looking for",
+  "Batman! (this commit has no parents)",
+  "Commit to me, I love you.",
+  "Commit without fear",
+  "There's no way I'm commiting this",
+  "What is it that you need again?"
+];
+
+const funnyCommit = () => {
+  const message = messages[Math.floor(Math.random() * messages.length)];
+  console.log(`\x1b[34m${message}\x1b[89m`);
+}
+
+module.exports = {
+  funnyCommit
+};
